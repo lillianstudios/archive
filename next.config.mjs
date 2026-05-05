21344b56dcf4622cf357f2c6cbc4f6802e81f9e9
lillian <lillian.malchan@gmail.com>
2026-05-05 22:31:12 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,17 @@ import { imageHosts } from './image-hosts.config.mjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  productionBrowserSourceMaps: true,
   images: {
     unoptimized: true,
     remotePatterns: imageHosts,
-    minimumCacheTTL: 60,
   },
+  // This helps bypass errors that usually stop the build
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
 };
 
 export default nextConfig;
